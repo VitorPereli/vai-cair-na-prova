bdd61803a609d34e97f8e6ef12e65ab2557757ef
Vitor Lima <vitorlima21.dev@gmail.com>
2026-09-22 19:11:41 -0300

    continuação

---
+++ funcao.ts
@@ -0,0 +1,25 @@
+function add (x: number, y:number) {
+    return x + y;
+}
+
+function sub (x:number, y:number) {
+    return x - y;
+}
+
+function mult (x:number, y:number) {
+    return x * y
+}
+
+function div (x:number, y:number) {
+    return x / y
+}
+
+let op = add(2, 10);
+let op2 = sub(2, 10);
+let op3 = mult(2, 10);
+let op4 = div(2, 10)
+
+console.log (op)
+console.log (op2)
+console.log (op3)
+console.log (op4)
